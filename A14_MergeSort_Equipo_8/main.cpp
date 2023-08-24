24c404501c8d02f2a968697c81e035d97fbe1fbe
danfupo03 <danfupo@outlook.com>
2023-08-24 16:22:40 -0600

    Added comments

--- A14_MergeSort_Equipo_8/main.cpp
+++ A14_MergeSort_Equipo_8/main.cpp
@@ -2,33 +2,39 @@
 
 using namespace std;
 
+/**
+ * @brief Merge two subarrays of arr[].
+ * 
+ * @param arr[] Array to be sorted.
+ * @param left First index of the array.
+ * @param middle Middle index of the array.
+ * @param right Last index of the array.
+ * 
+ * @return void
+ * 
+ * @complexity O(n)
+**/
 void merge(int arr[], int left, int middle, int right)
 {
     int n1 = middle - left + 1;
     int n2 = right - middle;
 
-    // Create temporary arrays to hold the subarrays
     int L[n1];
     int R[n2];
 
-    // Copy data to temporary arrays L[] and R[]
     for (int i = 0; i < n1; i++)
-    {
         L[i] = arr[left + i];
-    }
-    for (int j = 0; j < n2; j++)
-    {
-        R[j] = arr[middle + 1 + j];
-    }
 
-    // Merge the temporary arrays back into arr[left..right]
-    int i = 0;    // Initial index of first subarray
-    int j = 0;    // Initial index of second subarray
-    int k = left; // Initial index of merged subarray
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[middle + 1 + j];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
 
     while (i < n1 && j < n2)
     {
-        if (L[i] <= R[j])
+        if (L[i] >= R[j])
         {
             arr[k] = L[i];
             i++;
@@ -41,7 +47,6 @@ void merge(int arr[], int left, int middle, int right)
         k++;
     }
 
-    // Copy the remaining elements of L[], if any
     while (i < n1)
     {
         arr[k] = L[i];
@@ -49,7 +54,6 @@ void merge(int arr[], int left, int middle, int right)
         k++;
     }
 
-    // Copy the remaining elements of R[], if any
     while (j < n2)
     {
         arr[k] = R[j];
@@ -58,29 +62,36 @@ void merge(int arr[], int left, int middle, int right)
     }
 }
 
-// Merge Sort function
+/**
+ * @brief Sorts an array using merge sort.
+ * 
+ * @param arr[] Array to be sorted.
+ * @param left First index of the array.
+ * @param right Last index of the array.
+ * 
+ * @return void
+ * 
+ * @complexity O(n log n)
+**/
 void mergeSort(int arr[], int left, int right)
 {
     if (left < right)
     {
-        // Same as (left + right) / 2, but avoids overflow for large left and right
         int middle = left + (right - left) / 2;
 
-        // Recursively sort the first and second halves
         mergeSort(arr, left, middle);
         mergeSort(arr, middle + 1, right);
 
-        // Merge the sorted halves
         merge(arr, left, middle, right);
     }
 }
 
 int main()
 {
-    int arr[] = {5, 2, 4, 6, 1, 3};
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    cout << "Original Array: [ ";
+    cout << "\nOriginal Array: [ ";
 
     for (int i = 0; i < size; i++)
     {
@@ -95,6 +106,6 @@ int main()
     {
         cout << arr[i] << " ";
     }
-    cout << "]" << endl;
+    cout << "]\n" << endl;
     return 0;
 }
