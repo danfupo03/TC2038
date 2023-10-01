37e831710b74bb39e6c885342bed65a16ec87309
danfupo03 <danfupo@outlook.com>
2023-09-30 18:22:19 -0600

    Primera parte de la SP completa

--- ActInt1_Equipo_8/Equipo_8_main/main.cpp
+++ ActInt1_Equipo_8/Equipo_8_main/main.cpp
@@ -4,64 +4,77 @@
 
 using namespace std;
 
-void isSuffix(string s1, string s2)
+/**
+ * @brief Función que busca una cadena en otra.
+ * 
+ * @param pat 
+ * @param txt 
+ */
+void search(char* pat, char* txt)
 {
-    int n1 = s1.length(), n2 = s2.length();
-    bool isSuffix = true;
-    int startIndex = -1;
+    int M = strlen(pat);
+    int N = strlen(txt);
 
-    if (n1 > n2)
+    if (M > N)
     {
-        isSuffix = false;
+        cout << "La longitud del patrón es mayor que la de la cadena." << endl;
+        return;
     }
-    else
+
+    bool found = false;
+
+    for (int i = 0; i <= N - M; i++)
     {
-        for (int i = 0; i < n1; i++)
+        int j;
+
+        for (j = 0; j < M; j++)
         {
-            if (s1[n1 - i - 1] != s2[n2 - i - 1])
-            {
-                isSuffix = false;
+            if (txt[i + j] != pat[j])
                 break;
-            }
-            startIndex = n2 - i - 1;
+        }
+        if (j == M)
+        {
+            cout << "(true) " << i << endl;
+            found = true;
         }
     }
 
-    if (isSuffix)
+    if (!found)
     {
-        cout << "Yes, substring found at index: " << startIndex << endl;
-    }
-    else
-    {
-        cout << "No" << endl;
+        cout << "(false) Cadena no encontrada en la transmisión" << endl;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    ifstream inputFile;
+    ifstream inputFile1, inputFile2;
+    char s1[1000];
+    char s2[1000] = "";
+    char line[1000];
 
-    string s1, s2;
-
-    if (argc < 2)
+    if (argc < 3)
     {
-        cout << "Usage: " << argv[0] << " <input_file>" << endl;
+        cout << "Uso: " << argv[0] << " <input_file> <input_file2>" << endl;
         return 1;
     }
 
-    inputFile.open(argv[1]);
+    inputFile1.open(argv[1]);
+    inputFile2.open(argv[2]);
 
-    if (!inputFile)
+    if (!inputFile1 || !inputFile2)
     {
-        cout << "Error opening file" << endl;
+        cout << "Error abriendo el archivo" << endl;
         return 1;
     }
 
-    inputFile >> s1;
+    inputFile1 >> s1;
 
-    cout << "s1: " << s1 << endl;
+    while (inputFile2.getline(line, sizeof(line)))
+    {
+        strcat(s2, line);
+    }
 
-    // isSuffix(s1, s2);
+    search(s1, s2);
 
     return 0;
 }
